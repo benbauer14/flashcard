f8f688b3e4a242bd45fa7c4c8c8bcac44b5145c8
Ben Bauer <ben.bauer1@gmail.com>
2021-07-23 14:01:52 -0500

    cards flip, buttons do not work

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import tkinter
 import random
 import csv
 import time
+from tkinter.constants import COMMAND
 master_list = []
+englishword = ""
 # ----------LOAD CSV------------
 def onLoad():
     with open("data/french_words.csv") as file:
@@ -19,49 +21,61 @@ def randomWord():
     global master_list
     randomitem = random.randint(0, len(master_list) -1)
     words = master_list[randomitem]
+    canvas_card.itemconfig(canvas_text, text=words[0])
+    global englishword 
+    englishword= words[1]
+    # return words
 
-    return words
-# ------------UI---------------
+# ------------Right Click---------
+def correctClick():
+    global fliptimer
+    window.after_cancel(fliptimer)
+    randomWord()
+    # canvas_card.itemconfig(cards, image=front_card)
+    # canvas_card.itemconfig(canvas_text, fill="black")
+    fliptimer = window.after(3000,flipCard())
+
+# ------------Wrong Click---------
+def wrongClick():
+    window.after_cancel(fliptimer)
+    print("Wrong!")
+    randomWord()
+    canvas_card.itemconfig(cards, image=front_card)
+    canvas_card.itemconfig(canvas_text, fill="black")
+    fliptimer = window.after(3000,flipCard())
+
+# ------------Flip Card-------------
+def flipCard():
+    canvas_card.delete()
+    canvas_card.itemconfig(cards, image=back_card)
+    canvas_card.itemconfig(canvas_text, text=englishword, fill="white")
+
+# ------------UI--------------
+
 onLoad()
-print(randomWord()[1])
 window = tkinter.Tk()
 window.title("Flash Cards")
 window.minsize(width=800, height=650)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+fliptimer = window.after(3000, func=flipCard)
 
-canvas_card = tkinter.Canvas(width=800, height=550, bg=BACKGROUND_COLOR, highlightthickness=0 )
-flash_card = tkinter.PhotoImage(file="images/card_front.png")
-front = canvas_card.create_image(410, 300, image=flash_card)
-canvas_text = canvas_card.create_text(400, 300, text="Test", fill="black", font=("Arial", 30, "normal"))
-canvas_card.grid(column=1, row=1,columnspan=2, rowspan=10)
+canvas_card = tkinter.Canvas(width=800, height=550)
+front_card = tkinter.PhotoImage(file="images/card_front.png")
+back_card = tkinter.PhotoImage(file="images/card_back.png")
+cards = canvas_card.create_image(410, 300, image=front_card)
+canvas_text = canvas_card.create_text(400, 300, text="", fill="black", font=("Arial", 30, "normal"))
+canvas_card.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas_card.grid(column=0, row=0,columnspan=2)
 
-canvas_right = tkinter.Canvas(width=150, height=400, bg=BACKGROUND_COLOR, highlightthickness=0)
-correct = tkinter.PhotoImage(file="images/right.png")
-canvas_right.create_image(90,60,image=correct,)
-canvas_right.grid(column=1, row=11)
-
-canvas_wrong = tkinter.Canvas(width=200, height=400, bg=BACKGROUND_COLOR, highlightthickness=0)
 wrong = tkinter.PhotoImage(file="images/wrong.png")
-canvas_wrong.create_image(90,60,image=wrong)
-canvas_wrong.grid(column=2, row=11)
+wrong_button = tkinter.Button(image=wrong, highlightthickness=0, command=wrongClick)
+wrong_button.grid(column=0, row=1)
 
-game_over = False
-response = False
-while game_over == False:
-    word = randomWord()
-    canvas_card.delete(canvas_text)
-    canvas_text = canvas_card.create_text(400, 300, text=word[0], fill="black", font=("Arial", 30, "normal"))
-    canvas_card.update()
-    time.sleep(5)
-    canvas_card.delete(front)
-    flash_card = tkinter.PhotoImage(file="images/card_back.png")
-    back = canvas_card.create_image(410, 300, image=flash_card)
-    canvas_card.delete(canvas_text)
-    canvas_text = canvas_card.create_text(400, 300, text=word[1], fill="black", font=("Arial", 30, "normal"))
+correct = tkinter.PhotoImage(file="images/right.png")
+correct_button = tkinter.Button(image=correct, highlightthickness=0, command=correctClick)
+correct_button.grid(column=1, row=1)
+randomWord()
 
-    while response == False
-# time.sleep(20)
-# game_over = True
 
 
 
